e09b757e753505577986b403aabccad85f4e4952
Hongrui Zheng <billyzheng.bz@gmail.com>
2022-09-27 23:20:14 -0400

    initial update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='jax_mppi',
+      version='0.0.1',
+      author='Hongrui Zheng',
+      author_email='billyzheng.bz@gmail.com',
+      url='https://f1tenth.org',
+      package_dir={'': 'jax_mppi'},
+      install_requires=['jax']
+      )
