787f4b4651a913936f4b5235c4048e06b7ac81e0
Hongrui Zheng <billyzheng.bz@gmail.com>
2022-10-02 23:42:46 -0400

    structural updates

--- jax_mppi/mppi.py
+++ jax_mppi/mppi.py
@@ -31,6 +31,34 @@ import jax.numpy as jnp
 import jax.random as jrandom
 from st_drift import st_dyn_config, update_dyn_std
 
-class MPPI():
-    def __init__(self) -> None:
+
+class MPPI:
+    def __init__(
+        self,
+        dyn_fun,
+        running_cost,
+        nx,
+        noise_sigma,
+        num_samples=2048,
+        horizon=100,
+        terminal_state_cost=None,
+        lamb=1.0,
+        noise_mu=None,
+        u_min=None,
+        u_max=None,
+        U_init=None,
+        u_scale=1,
+    ) -> None:
+        # TODO: init dynamics, num sample, horizon, sigma init
+        pass
+
+    def plan(self, state):
+        # TODO: sample control inputs based on total cost dist
+        # TODO: rollout dynamics
+        pass
+
+    def _cal_costs(self):
+        # TODO: take rollout trajectories and calculate cost
+        # should be jitted and lax.scan wrapped
+        # TODO: update self total cost
         pass
